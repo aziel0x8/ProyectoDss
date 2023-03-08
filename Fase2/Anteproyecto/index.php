a8dd392dcce4ee7f21fc9c305aa2db43dd43fac3
aziel0x8 <78038166+aziel0x8@users.noreply.github.com>
2023-03-08 10:46:34 -0600

    registro-expresiones

---
+++ Fase2/Anteproyecto/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<!--Barra -->
+<div class="barra">
+	<blockquote><p>Banco De Agricultura</p></blockquote>
+</div>
+
+<!--Formulario-->
+<div class="wrapper">
+<div class="formulario">
+	<div class="encabezado"><h2>Login</h2><hr></div>
+	<form>	
+	  <label for="usuario">Usuario:</label>
+	  <input type="text" id="usuario"  name="usuario"><br><br>
+	  <label for="contra">Contraseña:</label>
+	  <input type="text" id="contra"  name="contra"><br>
+	  <img id="imagen" src="img/img.jpg" alt="">
+	  <div class="opciones">	
+	  <a href="recursos/olvidoContra.php" id="btncrear">Olvide contraseña</a>|
+	  <a href="recursos/registro.php">Crear Cuenta</a>
+	  </div>
+	  <input type="submit" value="LOGIN">
+	</form> 
+	</div>
+	</div>
+</body>
+</html>
